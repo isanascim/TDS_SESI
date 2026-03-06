5ca766bc8a50f05e59e14bfe336092b66cd6bab0
isanascim <isanascimentoescola@gmail.com>
2026-03-06 10:41:41 -0300

    Revert "Operadores aritméticos"
    This reverts commit 7b42082dfd755ea69ddb76f8b96a866b818ea123.

---
+++ Inicio/bloco.js
@@ -0,0 +1,14 @@
+{
+    console.log("Passo 01");
+    console.log("Passo 02");
+    
+    }
+    
+    {
+    console.log("Passo 01");
+    console.log("Passo 02");
+    }
+    
+    
+    console.log("Fim");
+    
