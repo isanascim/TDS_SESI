bce76074784da4511d3b5260ce63c21f65bf0006
isanascim <isanascimentoescola@gmail.com>
2026-03-06 11:39:16 -0300

    Operadores de atribuição e relacionais

---
+++ operadores/desafios_relacionais.js
@@ -0,0 +1,22 @@
+let a = 100;
+let b = 10;
+
+
+//imprimir no console 5 números verdadeiros(true)
+
+console.log(b < a); // Menor
+console.log(b << a); // Menor ou igual
+console.log(b <= a); // Menor ou igual
+console.log(a > b); // maior
+console.log(a != b); // diferente
+
+
+
+
+//imprimir no console 5 números falsos(false)
+
+console.log(b > a); // Maior
+console.log(a == b); // Igual
+console.log(b >= a); // Maior ou igual
+console.log(a <= b); // Menor ou igual
+console.log(a < b); // Menor
---
+++ operadores/relacionais2.js
@@ -0,0 +1,28 @@
+let a = 1; // Number
+let b = "1"; // String
+let c = "1"; // String
+
+// = atribuição
+// == compara somente  valor
+// === compara valor e tipo
+
+console.log(a == b);
+console.log(a === b);
+console.log(b == c);
+
+console.log(a != b);
+console.log(a !== b);
+console.log(b !== c);
+
+
+
+
+
+
+
+
+
+
+
+
+
