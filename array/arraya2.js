a3ba9354af1c7ec2551c2bdf5c663401a4c878c8
isanascim <isanascimentoescola@gmail.com>
2026-03-13 12:14:45 -0300

    Sobre o Arraya

---
+++ array/arraya2.js
@@ -0,0 +1,24 @@
+// Desafio; Inserir 5 novos índices com 5 nomes e imprimir na tela
+
+const nomes= [];
+nomes[0] = "Isabela";
+
+nomes[1] = "Isaac";
+
+nomes[2] = "Denise";
+
+nomes[3] = "Cleiton";
+
+nomes[4] = "Salete";
+console.log(nomes);
+
+
+
+
+
+
+
+
+
+
+
---
+++ array/desafio_arraya.js
@@ -0,0 +1,34 @@
+const numeros = [];
+
+//Inserir 10 números utilizando a função .push()
+// e imprimir na tela utilizando o formato: "Número: X"
+
+numeros.push(1);
+console.log("Número: " + 1)
+
+numeros.push(2);
+console.log("Número: " + 2)
+
+numeros.push(3);
+console.log("Número: " + 3)
+
+numeros.push(4);
+console.log("Número: " + 4)
+
+numeros.push(5);
+console.log("Número: " + 5)
+
+numeros.push(6);
+console.log("Número: " + 6)
+
+numeros.push(7);
+console.log("Número: " + 7)
+
+numeros.push(8);
+console.log("Número: " + 8)
+
+numeros.push(9);
+console.log("Número: " + 9)
+
+numeros.push(10);
+console.log("Número: " + 10)
