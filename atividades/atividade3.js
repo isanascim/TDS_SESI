d3f86650141ec079d28f945a3947170f5a53ddda
isanascim <isanascimentoescola@gmail.com>
2026-03-26 12:11:18 -0300

    Desafio 3

---
+++ atividades/atividade3.js
@@ -0,0 +1,9 @@
+let idade = 18;
+let convite = 1;
+let mensagem = convite >=1 ? "Acesso permitido" : "Acesso negado";
+
+if (idade >= 18) {
+    console.log("Você está autorizado!");
+} else {
+    console.log("Você não pode entrar!")
+}
