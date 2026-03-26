4f09534a1bc9e6506ac68724b982c9fbf8272da1
isanascim <isanascimentoescola@gmail.com>
2026-03-26 11:51:08 -0300

    Desafio 2

---
+++ atividades/atividade2.js
@@ -0,0 +1,6 @@
+var idadeCachorro = 13;
+let resultado;
+
+resultado = idadeCachorro * 7;
+
+console.log(`Se a idade de um humano for ${idadeCachorro}, sua idade equivale a ${resultado} anos em cachorro.`)
