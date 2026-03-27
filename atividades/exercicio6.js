8750b5e9d6f47e25436153155c518845fc648fc5
isanascim <isanascimentoescola@gmail.com>
2026-03-27 12:10:08 -0300

    desafio 5,6 e 7

---
+++ atividades/exercicio6.js
@@ -0,0 +1,17 @@
+let n1 = 10;
+let n2 = 10;
+let n3 = 10;
+let media = (n1 + n2 + n3) / 3;
+
+if(media >= 7.5) {
+
+    console.log("Aprovado!");
+
+} else if(media < 7.5 && media >= 5.0){
+    console.log("Recuperação!");
+
+}else{
+
+    console.log("Reprovado!");
+}
+console.media(media);
